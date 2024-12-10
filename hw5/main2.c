459265767b923735945c58f319feef0c3861e17f
marcoleung052 <marcoleung.cgu@gmail.com>
2024-12-10 15:04:13 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+int main()
+{
+    double PI=4.0f;
+    int PI_int=0;
+    bool tf=0;
+    for(int i=0;tf==0;i++){
+        PI=PI+pow(-1,i+1)*4/(3+i*2);
+        if((int)(PI*1000000+5)/10==314159){
+            printf("%d %.5f\n",i,PI);
+            tf=1;
+        }
+    }
+}
