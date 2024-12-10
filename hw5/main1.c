9bcf66066ce8ac91a0a842df36e38dc6a3edf444
marcoleung052 <marcoleung.cgu@gmail.com>
2024-12-10 13:35:02 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+int main()
+{
+    int n=7;
+    for(int i=1;i<=7;i++){
+        int m=n-(i-1);
+        printf("%*d",m,i);
+        for(int j=0;j<i-1;j++){
+            printf(" %d",i);
+        }
+        printf("\n");
+    }
+}
